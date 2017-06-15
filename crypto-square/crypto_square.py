962714ad2cbcd5906ea8cec08e8879020085c225
cmccandless <crm1994@gmail.com>
2017-06-15 17:49:47 -0500

    new solutions for python exercises

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,9 +1,14 @@
 from math import sqrt
+
+
 def encode(phrase):
-	phrase=[x for x in phrase.lower() if not x in " !,.?,'"]
-	n=len(phrase)
-	if n == 0: return ''
-	r=int(round(sqrt(n)))
-	c=int(round(n/r+0.5))
-	return ' '.join([''.join(x).strip() for x in 
-		zip(*[phrase[i:min(i+c,n)]+[' ']*max(0,i+c-n) for i in range(0,n,c)])])
+    phrase = [x for x in phrase.lower() if x not in " !,.?,'"]
+    n = len(phrase)
+    if n == 0:
+        return ''
+    r = int(round(sqrt(n)))
+    c = int(round(n / r + 0.5))
+    return ' '.join([''.join(x).strip()
+                     for x in zip(*[phrase[i:min(i + c, n)] +
+                                  [' '] * max(0, i + c - n)
+                                  for i in range(0, n, c)])])
