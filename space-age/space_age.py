20c91b266611a2080abd6cc50c8c27c9fb2f61b5
Corey McCandless <crm1994@gmail.com>
2017-10-06 11:53:50 -0500

    new revision for python/space-age

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,40 +1,23 @@
 EARTH_SECONDS = 31557600
-d = {'Earth': 1,
-     'Mercury': 0.2408467,
-     'Venus': 0.61519726,
-     'Mars': 1.8808158,
-     'Jupiter': 11.862615,
-     'Saturn': 29.447498,
-     'Uranus': 84.016846,
-     'Neptune': 164.79132}
-for k in d.keys():
-    d[k] *= EARTH_SECONDS
+FACTOR = {'earth': 1,
+          'mercury': 0.2408467,
+          'venus': 0.61519726,
+          'mars': 1.8808158,
+          'jupiter': 11.862615,
+          'saturn': 29.447498,
+          'uranus': 84.016846,
+          'neptune': 164.79132}
 
 
 class SpaceAge:
     def __init__(self, seconds):
         self.seconds = seconds
 
-    def on_earth(self):
-        return round(self.seconds / d['Earth'], 2)
-
-    def on_mercury(self):
-        return round(self.seconds / d['Mercury'], 2)
-
-    def on_venus(self):
-        return round(self.seconds / d['Venus'], 2)
-
-    def on_mars(self):
-        return round(self.seconds / d['Mars'], 2)
-
-    def on_jupiter(self):
-        return round(self.seconds / d['Jupiter'], 2)
-
-    def on_saturn(self):
-        return round(self.seconds / d['Saturn'], 2)
-
-    def on_uranus(self):
-        return round(self.seconds / d['Uranus'], 2)
-
-    def on_neptune(self):
-        return round(self.seconds / d['Neptune'], 2)
+    def __getattribute__(self, name):
+        if name.startswith('on_'):
+            planet = name[3:]
+            if planet in FACTOR:
+                def on_planet(seconds=self.seconds):
+                    return round(seconds / FACTOR[planet] / EARTH_SECONDS, 2)
+                return on_planet
+        return object.__getattribute__(self, name)
