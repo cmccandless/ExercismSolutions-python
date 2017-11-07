b0f6301fe69196161339468bf58bd5d5c48b164d
Corey McCandless <crm1994@gmail.com>
2017-11-07 13:52:49 -0600

    update prepare-commit-msg hook

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,7 +1,7 @@
 class Luhn:
     def __init__(self, number):
         self.number = number
-        terms = [(len(str(number)) - i - 1, int(x)) for 
+        terms = [(len(str(number)) - i - 1, int(x)) for
                  i, x in enumerate(str(number))]
         terms = [x if i % 2 == 0 else 2 * x for i, x in terms]
         self.__addends__ = [x if x < 10 else x - 9 for x in terms]
