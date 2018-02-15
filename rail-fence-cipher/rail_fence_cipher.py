73140fbd597c520cb6c034982955a5a4feedf5c1
Corey McCandless <crm1994@gmail.com>
2018-02-15 13:34:24 -0600

    new solutions for java exercises new revisions for java, python exercises

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -1,32 +1,32 @@
 def encode(str, n):
-    rails, r, d = ([''] * n, 0, 1)
-    for i, c in enumerate(str):
-        rails[r] = ''.join([rails[r], c])
-        r += d
-        if r < 0:
-            d = r = 1
-        elif r == n:
-            d, r = (-1, n - 2)
-    return ''.join(rails)
+    rails = [[] for _ in range(n)]
+    cap = 2 * n - 2
+    for i, ch in enumerate(str):
+        index = i % cap
+        if index >= n:
+            index = 2 * n - index - 2
+        rails[index].append(ch)
+    return ''.join(ch for r in rails for ch in r)
 
 
 def decode(str, n):
-    s, rails, r, d = (len(str), [0] * n, 0, 1)
-    for i, c in enumerate(str):
-        rails[r] += 1
-        r += d
-        if r < 0:
-            d = r = 1
-        elif r == n:
-            d, r = (-1, n - 2)
-    for r in range(len(rails)):
-        rails[r], str = (list(reversed(str[:rails[r]])), str[rails[r]:])
-    result, r, d = ('', 0, 1)
-    for _ in range(s):
-        result = ''.join([result, rails[r].pop()])
-        r += d
-        if r < 0:
-            d = r = 1
-        elif r == n:
-            d, r = (-1, n - 2)
-    return result
+    railCounts = [0] * n
+    cap = 2 * n - 2
+    for i in range(len(str)):
+        index = i % cap
+        if index >= n:
+            index = 2 * n - index - 2
+        railCounts[index] += 1
+    q = list(str)
+    rails = [
+        [q.pop(0) for _ in range(r)]
+        for r in railCounts
+    ]
+    decoded = []
+    d_append = decoded.append
+    for i in range(len(str)):
+        index = i % cap
+        if index >= n:
+            index = 2 * n - index - 2
+        d_append(rails[index].pop(0))
+    return ''.join(decoded)
