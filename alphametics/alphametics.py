cdbf970d2fc8e7057c37bda2ac183084b49221ae
Corey McCandless <corey.mccandless@atlasied.com>
2019-02-26 10:00:52 -0500

    alphametics: new revision

--- alphametics/alphametics.py
+++ alphametics/alphametics.py
@@ -1,19 +1,19 @@
 from itertools import permutations
 
 
-def sub(word, solution):
+def sub(solution, word):
     if solution[word[0]] == '0':
         raise ValueError()
-    return int(''.join(solution[w] for w in word))
+    return int(''.join(solution.get(w, w) for w in word))
 
 
 def solve(expr):
     words = [w for w in expr.split() if w not in ['+', '==']]
     letters = {ch for ch in expr if ch not in '+= '}
     for solution in (dict(zip(letters, perm)) for perm in
-                     permutations('0123456789', len(letters))):
+                     permutations('9876543210', len(letters))):
         try:
-            *left, right = (sub(word, solution) for word in words)
+            *left, right = (sub(solution, word) for word in words)
             if sum(left) == right:
                 return {k: int(v) for k, v in solution.items()}
         except ValueError:
